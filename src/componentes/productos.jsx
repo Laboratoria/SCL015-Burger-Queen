754d63731f3b5a427de064b02b26efdbd0e29fea
PaulyDiaz <pau.diazparedes@gmail.com>
2021-05-24 15:20:27 -0400

    prueba una App.js + producto.jsx

---
+++ src/componentes/productos.jsx
@@ -0,0 +1,26 @@
+import bebida from '../img/bebida.png';
+import bigmama from '../img/bigmama.png';
+import cafe from '../img/cafe.png';
+import doblebur from '../img/doblebur.png';
+import empanadas from '../img/empanadas.png';
+import ensalada from '../img/ensalada.png';
+import helado from '../img/helado.png';
+import jugo from '../img/jugo.png';
+import nuggets from '../img/nuggets.png';
+import papasfritas from '../img/papasfritas.png';
+import pollobur from '../img/pollobur.png';
+import quesoAZ from '../img/quesoAZ.png';
+import tacovegan from '../img/tacovegan.png';
+import te from '../img/te.png';
+
+function portada() {
+    return (
+            <div className ="botonprueba">
+                <p className = "frase">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis nihil quas sunt laboriosam dolore, alias, consectetur amet quod, ipsum aliquid sequi eum minima inventore possimus quam ea id sint nisi.</p>
+                <button>boton prueba</button>
+            </div>
+        
+        )
+  }
+  export default portada;
+  console.log("hola")
